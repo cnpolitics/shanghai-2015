80d63c565eb93928a58ec25869c42cab82c4fe7b
Zhengyu Qian <laerhsif@gmail.com>
2015-06-15 02:44:42 +0800

    Add the logic of cards' transition for desktop.

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,3 +11,10 @@ $('button, a').on('touchstart', function() {
 		$(this).removeClass('pressing');
 	});
 });
+
+var winWidth;
+$(function() { winWidth = $(window).width(); });
+$(window).on('resize', function() {
+	winWidth = $(window).width();
+	//console.log(winWidth);
+});
--- javascripts/card.js
+++ javascripts/card.js
@@ -5,18 +5,23 @@
 	$('.event-1 header').click(function() {
 		if ($('.event-1').hasClass('active')) {
 			// Pop off this card.
-			$('.event-1, .events-wrap').removeClass('active');
+			$('.event-1, .events-wrap').removeClass('active').css('height', '');
 			$('.card-wrap.center').removeClass('center');
 		} else {
 			// Pop off the current active card.
 			if ($('.events-wrap').hasClass('active')) {
-				$('.card-wrap.active').removeClass('active');
+				$('.card-wrap.active').removeClass('active').css('height', '');
 				$('.card-wrap.center').removeClass('center');
 			}
 			
 			// Pop on the clicked card and re-order the other cards.
 			$('.event-1, .events-wrap').addClass('active');
 			$('.event-2').addClass('center');
+			if (winWidth >= 960) {
+				setTimeout(function() {
+					$('.event-1').css('height', 'auto');
+				}, 500);
+			}
 		}
 	});
 	
@@ -24,18 +29,23 @@
 	$('.event-2 header').click(function() {
 		if ($('.event-2').hasClass('active')) {
 			// Pop off this card.
-			$('.event-2, .events-wrap').removeClass('active');
+			$('.event-2, .events-wrap').removeClass('active').css('height', '');
 			$('.card-wrap.center').removeClass('center');
 		} else {
 			// Pop off the current active card.
 			if ($('.events-wrap').hasClass('active')) {
-				$('.card-wrap.active').removeClass('active');
+				$('.card-wrap.active').removeClass('active').css('height', '');
 				$('.card-wrap.center').removeClass('center');
 			}
 			
 			// Pop on the clicked card and re-order the other cards.
 			$('.event-2, .events-wrap').addClass('active');
 			$('.event-1').addClass('center');
+			if (winWidth >= 960) {
+				setTimeout(function() {
+					$('.event-2').css('height', 'auto');
+				}, 500);
+			}
 		}
 	});
 	
@@ -43,18 +53,23 @@
 	$('.event-3 header').click(function() {
 		if ($('.event-3').hasClass('active')) {
 			// Pop off this card.
-			$('.event-3, .events-wrap').removeClass('active');
+			$('.event-3, .events-wrap').removeClass('active').css('height', '');
 			$('.card-wrap.center').removeClass('center');
 		} else {
 			// Pop off the current active card.
 			if ($('.events-wrap').hasClass('active')) {
-				$('.card-wrap.active').removeClass('active');
+				$('.card-wrap.active').removeClass('active').css('height', '');
 				$('.card-wrap.center').removeClass('center');
 			}
 			
 			// Pop on the clicked card and re-order the other cards.
 			$('.event-3, .events-wrap').addClass('active');
 			$('.event-1').addClass('center');
+			if (winWidth >= 960) {
+				setTimeout(function() {
+					$('.event-3').css('height', 'auto');
+				}, 500);
+			}
 		}
 	});
 	
