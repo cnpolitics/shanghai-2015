79894cca170e319d25bcb9058ffadd38e6e076a1
Zhengyu Qian <laerhsif@gmail.com>
2015-10-31 19:26:37 +0800

    Update comments for license.

--- javascripts/_libs.js
+++ javascripts/_libs.js
@@ -1,4 +1,4 @@
-/* @preserve
+/* @license
  * jQuery v2.1.4 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license
  * FastClick | ftlabs-jsv2 | (c) The Financial Times Limited | MIT License
  */
